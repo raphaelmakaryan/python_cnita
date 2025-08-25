8dd94ebf9d22b4779d5c4c72e89442052f965b52
raphaelmakaryan <contact@raphaelmakaryan.fr>
2025-08-25 09:31:50 +0200

    Fin de l'exercice de notion de base

---
+++ 1.1.py
@@ -0,0 +1,5 @@
+n = 1;
+for i in range(n):
+    x = input("Donnez votre nom : ")
+    if x:
+        print("Hello",x)
---
+++ test/notion.py
@@ -0,0 +1,19 @@
+for i in [0,1]:
+    print("valeur : ", i)
+print("fin")
+
+addition = 4+5
+print(addition)
+
+premaffect = 2
+print(premaffect)
+premaffect = 5
+print(premaffect)
+
+tab = list(range(5))
+print(tab)
+
+print(tab[3])
+
+long = len(tab)
+print(long)
