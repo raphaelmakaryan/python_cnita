f3d3058dc6210588a3b37d625d585a9241aad58a
raphaelmakaryan <contact@raphaelmakaryan.fr>
2025-08-25 10:39:16 +0200

    Commencement des exercices du document 1.2

---
+++ 1.2.py
@@ -0,0 +1,51 @@
+from sympy import false
+
+
+def fizzBuzz():
+    # 1 a 100 | Multiple de 3 = Fizz | Multiple de 5 = Buzz | Mutiple de 3 et 5 = FizzBuzz
+    for i in range(1, 100):
+        if i % 3 == 0 or i == 3:
+            print("Fizz")
+        elif i % 5 == 0 or i == 5:
+            print("Buzz")
+        elif i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        else:
+            print(i)
+    print("Fin de la boucle")
+
+
+def leapYears(n):
+    # Toute les annés sont divisible par 400 sont bissextiles | Tout annés div par 4 et pas 100 sont bissextiles
+    # Toute les annés divisible par 100 mais pas 400 sont pas bixesiiles |  tout annés pas difibie par 4 sont pas bissetile
+    for i in range(n):
+        x = int(input("Donnez une année : "))
+        if x % 400 == 0 or (x % 4 == 0 and x % 100 != 0):
+            print("Bissextile")
+        elif (x % 100 == 0 and x % 400 != 0) or x % 4 != 0:
+            print("Pas bissextile")
+        else:
+            print("Soucis pour l'année :", x)
+
+
+def diamond():
+    tableAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+                     "U", "V", "W", "X", "Y", "Z"]
+    valuePosition = 1
+    for i in range(1):
+        valueUser = input("Donnez une lettre : ")
+        for j in range(len(tableAlphabet)):
+            if tableAlphabet[j] == valueUser:
+                valuePosition = j
+                break
+            else:
+                valuePosition = 0
+
+        if (valuePosition != 0):
+            for k in range(valuePosition + 1):
+
+                print(tableAlphabet[k])
+            for l in range(valuePosition - 1, -1, -1):
+                print(tableAlphabet[l])
+
+diamond()
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # python_cnita
 Cours Python | Campus Numérique in the Alps
+
+Commande pour lancé : 
+
+- `sudo apt install python3-venv`
+- `python3 -m venv .venv`
+- `source .venv/bin/activate`
+- `pip install requests`
+- `pip install -U pytest`
+
+ou 
+
+`sudo apt-get update  
+sudo apt-get install python3-pytest`
